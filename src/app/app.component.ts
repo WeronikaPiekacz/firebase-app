54eddddbbbe2a00cbf70604b993742aac5eed059
Werka Piekacz <weronika.piekacz@interia.pl>
2023-02-15 21:40:16 +0100

    FirstApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,20 @@
 import { Component } from "@angular/core";
+import { environment } from "../environments/environment";
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { Database, getDatabase, ref, set, onValue } from "firebase/database";
+import { DocumentData, Firestore } from "@firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  setDoc,
+} from "firebase/firestore/lite";
+import { randomUUID } from "crypto";
+import { Guid } from "guid-ts";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -6,4 +22,65 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "firebase-app";
+  app: FirebaseApp;
+  db: Database;
+  username = "";
+  message = "";
+  private firestore: Firestore | Promise<Firestore>;
+  protected form: FormGroup = new FormGroup({});
+  protected results:
+    | { name: string; message: string; date: Date }[]
+    | undefined;
+
+  constructor(private fb: FormBuilder) {
+    this.app = initializeApp(environment.firebase);
+    this.db = getDatabase(this.app);
+    this.firestore = getFirestore(this.app);
+  }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      name: [null, [Validators.required, Validators.maxLength(15)]],
+      message: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(150),
+        ],
+      ],
+    });
+
+    this.getData();
+    console.log(this.results);
+  }
+
+  onChatSubmit() {
+    return Promise.resolve(this.firestore).then((db) => {
+      const ref = doc(db, "chat", Guid.newGuid().toString());
+      return setDoc(ref, {
+        username: this.form.controls["name"].value,
+        message: this.form.controls["message"].value,
+        date: new Date().toUTCString(),
+      }).then(() => {
+        this.form.controls["message"].setValue("");
+        this.getData();
+      });
+    });
+  }
+
+  public getData(): Promise<any> {
+    return Promise.resolve(this.firestore).then((db) => {
+      return getDocs(collection(db, "chat")).then(
+        (results) =>
+          (this.results = results.docs
+            .map((result) => ({
+              name: result.get("username"),
+              message: result.get("message"),
+              date: new Date(result.get("date")),
+            }))
+            .sort((a, b) => b.date.getTime() - a.date.getTime()))
+      );
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,21 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { AngularFireModule } from "@angular/fire/compat";
 import { AngularFireAuthModule } from "@angular/fire/compat/auth";
 import { AngularFireDatabaseModule } from "@angular/fire/compat/database";
 import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
 import { AngularFireStorageModule } from "@angular/fire/compat/storage";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -22,6 +33,17 @@ import { AppComponent } from "./app.component";
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    CommonModule,
+    MatTableModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatButtonModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
