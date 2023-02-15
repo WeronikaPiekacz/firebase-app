7d9142e73b071e40ff527e1a80e39aebda48b4da
Werka Piekacz <weronika.piekacz@interia.pl>
2023-02-15 19:33:42 +0100

    Connection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBYP4KF8hCgxGXauJRRgKyxkSrj0Stgeb0",
+    authDomain: "werka-firebase-app.firebaseapp.com",
+    projectId: "werka-firebase-app",
+    storageBucket: "werka-firebase-app.appspot.com",
+    messagingSenderId: "739314615575",
+    appId: "1:739314615575:web:c992d5685563205fc52352",
+    measurementId: "G-TEGDC2FKP8",
+  },
 };
